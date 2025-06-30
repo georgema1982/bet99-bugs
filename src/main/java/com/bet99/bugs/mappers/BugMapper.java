691d0584133d8b9107af9795c7b4504541aa44fe
georgema1982 <george.ma1982@gmail.com>
2025-06-30 00:36:05 -0600

    Add Liquibase configuration and initial schema for bug management

--- src/main/java/com/bet99/bugs/mappers/BugMapper.java
+++ src/main/java/com/bet99/bugs/mappers/BugMapper.java
@@ -1,5 +1,11 @@
 package com.bet99.bugs.mappers;
 
-public interface BugMapper {
+import com.bet99.bugs.models.BugDto;
+import com.bet99.bugs.entities.BugEntity;
+import org.mapstruct.Mapper;
 
+@Mapper(componentModel = "spring")
+public interface BugMapper {
+    BugDto toDto(BugEntity entity);
+    BugEntity toEntity(BugDto dto);
 }
